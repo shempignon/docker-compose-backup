13aa8067f012675f93e3531bc56109f8602938cf
Shempignon <nicolas@pointndots.com>
2020-04-23 02:22:44 +0200

    Setup configuration

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use serde_derive::Deserialize;
+
+#[derive(Deserialize)]
+struct Config {
+  backup_directory: String,
+  projects: Vec<BackupConfig>
+}
+
+#[derive(Deserialize)]
+struct BackupConfig {
+  service: String,
+  docker_compose: String,
+  path: String,
+}
+
 fn main() {
     println!("Hello, world!");
 }
