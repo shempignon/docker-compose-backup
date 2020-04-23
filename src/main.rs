461f317828a837fcb3f73b5d3073c417d3ec70be
Shempignon <nicolas@pointndots.com>
2020-04-23 14:15:04 +0200

    Read config from args

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use serde_derive::Deserialize;
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 struct Config {
   backup_directory: String,
   projects: Vec<BackupConfig>
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 struct BackupConfig {
   service: String,
   docker_compose: String,
@@ -14,5 +17,27 @@ struct BackupConfig {
 }
 
 fn main() {
-    println!("Hello, world!");
+  let args: Vec<String> = env::args().collect();
+  
+  if args.len() < 2 {
+    panic!("You need to provide the config.toml file as first parameter");
+  }
+
+  let config = get_config(&args[1]).unwrap();
+
+  println!("{:?}", config);
+}
+
+fn stringify<T>(err: T) -> String where T: ToString {
+  err.to_string()
+}
+
+fn get_config(path: &str) -> Result<Config, String> {
+  let mut file = File::open(path).map_err(stringify)?;
+  let mut contents = String::new();
+  file.read_to_string(&mut contents).map_err(stringify)?;
+
+  let config: Config = toml::from_str(&contents).map_err(stringify)?;
+
+  Ok(config)
 }
